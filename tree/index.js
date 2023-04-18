4630e8d9c8d97c84173a3edd65adeadded63ace2
PRANAVBHALE <bhalepranav25@gmail.com>
2023-04-18 18:15:46 +0530

    implementation of dfs method for tree

--- tree/index.js
+++ tree/index.js
@@ -9,20 +9,46 @@ class Node{
     }
 
     removeNode(data){
-        this.children.filter(node => node.data !== data)
+        this.children = this.children.filter(node => node.data !== data)
     }
 }
-
-let rootNode = new Node("root")
-rootNode.addNode(1)
-rootNode.addNode(2)
-rootNode.addNode(3)
-rootNode.addNode(4)
-rootNode.addNode(5)
-console.log(rootNode)
 
 class Tree{
     constructor(){
         this.root = null
     }
+
+    dfs(fn){
+
+        let arr = [this.root]
+
+        while(arr.length){
+
+            let node = arr.shift()
+
+            arr.unshift(...node.children)
+            fn(node)
+        }
+    }
+}
+
+// let tree = new Tree()
+// tree.root = new Node("root")
+// tree.root.addNode("a")
+// tree.root.addNode("b")
+// tree.root.addNode("c")
+// tree.root.children[0].addNode("d")
+// console.log('tree',tree)
+
+// let letters = []
+
+// tree.dfs(node => {
+//     letters.push(node.data)
+// })
+
+// console.log("letters--->",letters)
+
+module.exports = {
+    Tree,
+    Node
 }
