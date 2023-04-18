466573452433855a7c0836e852a849a7c0ec9699
PRANAVBHALE <bhalepranav25@gmail.com>
2023-04-18 18:16:41 +0530

    dfs example demo

---
+++ tree/dfs.js
@@ -0,0 +1,18 @@
+const { Tree,Node } = require(".");
+
+
+let tree = new Tree()
+tree.root = new Node("root")
+tree.root.addNode("a")
+tree.root.addNode("b")
+tree.root.addNode("c")
+tree.root.children[0].addNode("d")
+console.log('tree',tree)
+
+let letters = []
+
+tree.dfs(node => {
+    letters.push(node.data)
+})
+
+console.log("letters--->",letters)
